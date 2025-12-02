65a6d5505af9816871d7dad5c1a18371c880675a
maqsadjon2024 <maqsadjonqurbondurdiev58@gmail.com>
2025-12-02 17:15:13 +0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,24 @@
+# 1-misol
+string_uzunligi = lambda s: len(s)
+
+print(string_uzunligi("Maqsadjon"))
+
+# 2-misol
+tekshir_son = lambda x: "musbat" if x > 0 else ("manfiy" if x < 0 else "nol")
+
+print(tekshir_son(17))
+
+# 3-misol
+uchburchak_yuzasi = lambda asos, balandlik: 0.5 * asos * balandlik
+
+print(uchburchak_yuzasi(17,12))
+
+# 4-misol
+juftmi = lambda x: x % 2 == 0
+
+print(juftmi(18))
+
+# 5-misol
+katta_10 = lambda x: x > 10
+
+print(katta_10(17))
